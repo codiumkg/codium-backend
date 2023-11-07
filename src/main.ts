10294caf8c600f0fdb1778e0c7f931ace194ab79
akmatoff <akmatoff.exe@gmail.com>
2023-11-08 01:24:05 +0600

    Swagger, remove API prefix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api', {
-    exclude: ['auth/login', 'auth/signup', 'auth/status'],
-  });
   app.enableCors();
+
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
