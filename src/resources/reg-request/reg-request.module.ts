94edc313a08a7add8072347bf637b7ca9b4efdaf
akmatoff <akmatoff.exe@gmail.com>
2023-11-08 02:03:29 +0600

    Prisma service in reg requests fix

--- src/resources/reg-request/reg-request.module.ts
+++ src/resources/reg-request/reg-request.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RegRequestService } from './reg-request.service';
 import { RegRequestController } from './reg-request.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [RegRequestController],
-  providers: [RegRequestService],
+  providers: [RegRequestService, PrismaService],
 })
 export class RegRequestModule {}
