778454f00958111bd8899dadf356df6ba90b6bd0
akmatoff <akmatoff.exe@gmail.com>
2024-02-21 23:20:49 +0600

    protect topic content controller

--- src/resources/topic-content/topic-content.controller.ts
+++ src/resources/topic-content/topic-content.controller.ts
@@ -6,30 +6,41 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { TopicContentService } from './topic-content.service';
 import { CreateTopicContentDto } from './dto/create-topic-content.dto';
 import { UpdateTopicContentDto } from './dto/update-topic-content.dto';
+import { RolesGuard } from 'src/guards/roles-guard/roles-guard.guard';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { HasRoles } from '../auth/has-roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('topic-content')
 export class TopicContentController {
   constructor(private readonly topicContentService: TopicContentService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HasRoles(Role.ADMIN, Role.MANAGER)
   @Post()
   create(@Body() createTopicContentDto: CreateTopicContentDto) {
     return this.topicContentService.create(createTopicContentDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.topicContentService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.topicContentService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HasRoles(Role.ADMIN, Role.MANAGER)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +49,8 @@ export class TopicContentController {
     return this.topicContentService.update(+id, updateTopicContentDto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @HasRoles(Role.ADMIN, Role.MANAGER)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.topicContentService.remove(+id);
