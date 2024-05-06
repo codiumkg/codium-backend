255ad56b1439c3f793cf2a5420eeddd53bbcdb32
akmatoff <akmatoff.exe@gmail.com>
2024-05-06 23:08:38 +0600

    Include answers in topic content

--- src/resources/topic-content/topic-content.service.ts
+++ src/resources/topic-content/topic-content.service.ts
@@ -17,14 +17,22 @@ export class TopicContentService {
     return this.prismaService.topicContent.findMany({
       ...(topicId && { where: { topicId } }),
       orderBy: { orderNumber: 'asc' },
-      include: { topic: true, lecture: true, task: true },
+      include: {
+        topic: true,
+        lecture: true,
+        task: { include: { answers: true } },
+      },
     });
   }
 
   findOne(id: number) {
     return this.prismaService.topicContent.findFirst({
       where: { id },
-      include: { topic: true, lecture: true, task: true },
+      include: {
+        topic: true,
+        lecture: true,
+        task: { include: { answers: true } },
+      },
     });
   }
 
