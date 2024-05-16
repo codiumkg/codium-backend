92512474365842f495b5e8f9b5665f3e4571612e
akmatoff <akmatoff.exe@gmail.com>
2024-05-16 18:12:27 +0600

    Include task user answer service in task module

--- src/resources/task/task.module.ts
+++ src/resources/task/task.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from 'src/prisma.service';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { AnswerService } from '../answer/answer.service';
+import { TaskUserAnswerService } from '../task-user-answer/task-user-answer.service';
 
 @Module({
   controllers: [TaskController],
-  providers: [TaskService, PrismaService, AnswerService],
+  providers: [TaskService, PrismaService, AnswerService, TaskUserAnswerService],
 })
 export class TaskModule {}
